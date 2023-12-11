cb92761cf9d08d9b3b8d3a7da5c58207434bcbfc
adrian <adrianmartinez.progra@gmail.com>
2023-12-11 00:28:24 -0600

    modificacion concenso.py

--- concenso.py
+++ concenso.py
@@ -3,26 +3,22 @@ import threading
 import pickle
 
 class ConsensusNode:
-    def __init__(self, node_id, port):
+    def __init__(self, node_id, port, initial_data):
         self.node_id = node_id
         self.port = port
-        self.data = {}  # Base de datos del nodo
+        self.data = initial_data
         self.peers = [("localhost", p) for p in range(8000, 8005) if p != port]
         self.current_command = None
 
     def start(self):
-        # Iniciar servidor en un hilo separado
         server_thread = threading.Thread(target=self.start_server)
         server_thread.start()
-
-        # Iniciar loop principal para manejar comandos
         self.run()
 
     def start_server(self):
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.bind(("localhost", self.port))
         server.listen(5)
-
         while True:
             client, addr = server.accept()
             data = client.recv(1024)
@@ -33,20 +29,9 @@ class ConsensusNode:
 
     def run(self):
         while True:
-            command = input(f"Node {self.node_id}: Enter command ('exit' to quit): ")
-            if command == 'exit':
-                break
-
-            # Envía el comando a todos los nodos
-            self.broadcast_command(command)
-
-            # Espera a que todos los nodos confirmen el comando
-            consensus_reached = self.wait_for_consensus()
-
-            if consensus_reached:
-                print(f"Consensus reached. Command '{command}' applied to all nodes.")
-            else:
-                print("Consensus not reached. Retrying...")
+            # Aquí puedes solicitar al usuario que ingrese el comando de actualización
+            update_command = {"101": {"cantidad": 55}, "102": {"precio": 1100}}
+            self.broadcast_command(update_command)
 
     def broadcast_command(self, command):
         self.current_command = command
@@ -57,23 +42,18 @@ class ConsensusNode:
             client.close()
 
     def handle_command(self, command):
-        # Aplica el comando a la base de datos local
-        # Aquí deberías implementar la lógica específica para tu aplicación
         print(f"Node {self.node_id} applying command: {command}")
-        self.data = command
-
-    def wait_for_consensus(self):
-        # Espera a que todos los nodos confirmen el comando
-        while True:
-            if all(self.current_command == node.data for node in self.get_other_nodes()):
-                return True
-
-    def get_other_nodes(self):
-        # Devuelve todos los nodos excepto el actual
-        return [node for node in nodes if node.node_id != self.node_id]
+        for item_id, update_data in command.items():
+            if item_id in self.data:
+                self.data[item_id].update(update_data)
 
 if __name__ == "__main__":
-    nodes = [ConsensusNode(i, 8000 + i) for i in range(5)]
+    initial_data = {
+        "101": {"articulo": "Playera", "categoria": "Ropa", "precio": 500, "cantidad": 50, "Inv1": 0, "Inv2": 0, "Inv3": 0, "Inv4": 0, "Inv5": 0},
+        # ... (otros artículos)
+    }
+
+    nodes = [ConsensusNode(i, 8000 + i, initial_data.copy()) for i in range(5)]
 
     for node in nodes:
         node.start()
